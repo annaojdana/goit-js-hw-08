0768ed017becdb09d6f107add39cc0a93bfa3205
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-25 12:28:01 +0200

    set current time - without trottle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,50 @@
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
 const player = new Player(iframe);
+let currentTime = load(LOCALSTORAGE_KEY);
 
-player.on('play', function() {
-        console.log('played the video!');
-});
-player.on('timeupdate', function(data) {
-        
 
-});
+player.on('timeupdate', saveTime);
 
-player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-});
+
+player.setCurrentTime(currentTime).then(setTime).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});;
+
+function saveTime(data) {
+    const seconds = data.seconds;
+    save(LOCALSTORAGE_KEY, seconds);   
+}
+
+function save (key, value){
+    try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
+
+function load (key){
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+function setTime (seconds) {
+    // seconds = the actual time that the player seeked to
+    console.log(seconds);
+}
