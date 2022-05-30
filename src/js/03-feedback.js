42d3780a30f0c5cf8c2274ba34ee40563cc4dd21
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-30 18:07:07 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+// import throttle from 'lodash.throttle'
+import './storage'
 
+const form = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = "feedback";
+
+save("test", "test");
