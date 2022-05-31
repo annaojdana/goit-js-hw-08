c81097f12a25f4fcf88bfb0e2c73552dc99a699e
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-31 13:11:41 +0200

    cosmetic changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-import {save,load} from'./storage'
+import { save, load } from './storage';
 
+// variables
 const iframe = document.querySelector('iframe');
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 const player = new Player(iframe);
@@ -9,21 +10,19 @@ let currentTime = load(LOCALSTORAGE_KEY);
 
 
 player.on('timeupdate', throttle(data => saveTime(data), 1000));
-
-
 player.setCurrentTime(currentTime).then(setTime).catch(errorSetTime);
 
+// functions
 function saveTime(data) {
     const seconds = data.seconds;
     save(LOCALSTORAGE_KEY, seconds);   
 }
 
-
-
 function setTime (seconds) {
     // seconds = the actual time that the player seeked to
-    console.log(seconds);
+    console.log(`Last time:${seconds}s`);
 }
+
 function errorSetTime (error) {
     switch (error.name) {
         case 'RangeError':
