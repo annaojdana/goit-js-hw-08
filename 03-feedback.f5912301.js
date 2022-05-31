ad8f18219618610fb2769d0f0bcbd6f61703025a
annaojdana <35433693+annaojdana@users.noreply.github.com>
2022-05-31 08:21:58 +0000

    Deploying to gh-pages from @ annaojdana/goit-js-hw-08@20120d730b40b030a1d5f622bda6ddaf078c54c7 🚀

---
+++ 03-feedback.f5912301.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form");!function(e,t){try{const r=JSON.stringify(t);localStorage.setItem(e,r)}catch(e){console.error("Set state error: ",e.message)}}("test","test");
+//# sourceMappingURL=03-feedback.f5912301.js.map
