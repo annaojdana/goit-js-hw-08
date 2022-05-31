20120d730b40b030a1d5f622bda6ddaf078c54c7
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-31 10:21:22 +0200

    searching bugs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,46 @@
-import throttle from 'lodash.throttle'
-import {save, load, remove} from './storage'
+// import throttle from 'lodash.throttle';
+import { save, remove } from './storage';
 
 const form = document.querySelector(".feedback-form");
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
+save("test", "test");
 
-form.addEventListener("input", follow);
+// form.addEventListener("input", handleClick);
 
-form.addEventListener("submit", handleSubmit);
+// form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const {
-    elements: { email, message }
-  } = event.currentTarget;
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, message }
+//   } = event.currentTarget;
 
-  if (email.value === "" || message.value === "") {
-    return alert("Please fill in all the fields!");
-  }
+//   if (email.value === "" || message.value === "") {
+//     return alert("Please fill in all the fields!");
+//   }
 
-  console.log(`email: ${email.value}, message: ${message.value}`);
-  remove(LOCALSTORAGE_KEY);  
-  event.currentTarget.reset();
-}
-function follow (event) {
-  const {
-    elements: { email, message }
-  } = event.currentTarget;
-  const data = {
-    email: email.value,
-    message: message.value,
-    }
+//   console.log(`email: ${email.value}, message: ${message.value}`);
+//   remove(LOCALSTORAGE_KEY);  
+//   event.currentTarget.reset();
+// }
+
+// function follow (data) {
     
-    throttle((d) => save(LOCALSTORAGE_KEY, data), 500);
-}
+//   const {
+//     elements: { email, message }
+//   } = data.currentTarget;
+//     const iputsData = {
+//         email: email.value,
+//         message: message.value,
+//     }; 
+   
+//     save(LOCALSTORAGE_KEY, iputsData);
+
+// }
+
+// const handleClick = (event) => {
+//   console.log("event: ", event);
+//   console.log("event type: ", event.type);
+//   console.log("currentTarget: ", event.currentTarget);
+// };
