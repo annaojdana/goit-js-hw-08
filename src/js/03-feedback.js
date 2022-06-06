4a4ec355ab9f1881d55b0131038ce549663003f1
Anna Ojdana <anna.ojdana@gmail.com>
2022-06-06 14:13:27 +0200

    serching for  bugs - task02

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,20 +3,22 @@ import { save, load, remove } from './storage';
 
 // variables
 const form = document.querySelector(".feedback-form");
+
 const LOCALSTORAGE_KEY = "feedback-form-state";
 const throttleSave = throttle((event) => saveChanges(event), 500);
 
 // addEventListeners
-window.addEventListener('load', getData);
 form.addEventListener("input", throttleSave);
 form.addEventListener("submit", handleSubmit);
+
+
+getData(); 
+
 
 // Functions
 function handleSubmit(event) {
   event.preventDefault();
-  const {
-    elements: { email, message }
-  } = event.currentTarget;
+  const  { email, message } = event.currentTarget.elements;
 
   if (email.value === "" || message.value === "") {
     return alert("Please fill in all the fields!");
@@ -28,21 +30,24 @@ function handleSubmit(event) {
 }
 
 function saveChanges (event) { 
-  const {elements: { email, message }} = event.currentTarget;
+  const { email, message } = event.currentTarget.elements;
   const iputsData = {
-    emailContent: email.value,
-    messageContent: message.value,
+    email: email.value,
+    message: message.value,
   }; 
   save(LOCALSTORAGE_KEY, iputsData);
 };
 
 function getData() { 
-  let currentData = load(LOCALSTORAGE_KEY);
-  if (currentData !== undefined) {
-    const { emailContent, messageContent } = currentData;
-    const { elements: { email, message } } = form;
-    email.value = emailContent;
-    message.value = messageContent;
-  };
+  const storedData = load(LOCALSTORAGE_KEY);
+    if (storedData === null) {
+        return
+    };
+
+    const { email, message } = form.elements;
+    
+    email.value = storedData.email;
+    message.value = storedData.message;
+  
 };
 
